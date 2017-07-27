f14ee464c6ffdf9340024bc071941bd93fecd430
Eugene Kardash <eugeneka@gmail.com>
2017-07-27 14:15:38 -0700

    Aeon switch usability
    Removed unnecessary strobe effect, improved settings screen (human English params). Tested at my home for 6 months.

--- aeonSwitch.groovy
+++ aeonSwitch.groovy
@@ -1,19 +1,19 @@
-/* AEON specific micro driver 1.4
+/* AEON specific micro driver 1.5
  *
  * Variation of the stock SmartThings "Dimmer-Switch" and twack's improved dimmer
  *	--auto re-configure after setting preferences
- *	--alarm indicator capability (using AEON hardware blink function)
  *	
  *
  * Includes:
  *	preferences tile for setting:
  * 		reporting functions (parameter 80)	[ 0:off, 1:hail, 2:report ] set to "Report" for fastest physical updates from the device
- *		control switch type (parameter 120)	[ 0:momentary, 1:toggle, 2:three way] (2 isn't tested, not sure how its suppposed to work)
- *		preconfigured blinker modes			[Blink, Flasher, Strobe]
+ *		control switch type (parameter 120)	[ 0:momentary, 1:toggle, 2:three way]
  *		
  * Mike Maxwell
  * madmax98087@yahoo.com
  * 2014-12-06
+ * Eugene Kardash
+ * 2016-10-27
  *
 	change log
     1.1 2014-12-08
@@ -24,6 +24,8 @@
     	-bug fixes, complete parser re-write
     1.4 2014-12-26
     	-hanked flakey turning states
+    1.5 2016-10-27
+    	-removed blinker, improved definitions of params 80 and 120
     
 	AEON G2 
 	0x20 Basic
@@ -42,21 +44,19 @@
 
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "aeonSwitch", namespace: "mmaxwell", author: "mike maxwell") {
+	definition (name: "AeonSwitch", namespace: "EugeneKardash", 
+    	author: "eugene@kardash.net") {
 		capability "Actuator"
 		capability "Switch"
 		capability "Refresh"
 		capability "Sensor"
         capability "Alarm" 
-        command "flash", ["string"]  //blink,flasher,strobe
-        //aeon S2 switch (DSC26103-ZWUS)
-        fingerprint deviceId: "0x1001", inClusters: "0x25,0x27,0x2C,0x2B,0x70,0x85,0x72,0x86,0xEF,0x82"
+        inClusters: "0x25,0x27,0x2C,0x2B,0x70,0x85,0x72,0x86,0xEF,0x82"
 
 	}
     preferences {
-       	input name: "param80", type: "enum", title: "State change notice:", description: "Type", required: true, options:["Off","Hail","Report"]
-        input name: "param120", type: "enum", title: "Set trigger mode:", description: "Switch type", required: true, options:["Momentary","Toggle","Three Way"]
-        input name: "blinker", type: "enum", title: "Set blinker mode:", description: "Blinker type", required: false, options:["Blink","Flasher","Strobe"]
+       	input name: "Notifications_param_80", type: "enum", title: "State Change Notifications (param 80)", description: "(0:nothing, 1:hail CC, 2:basic CC report, other: ignore)", required: true, options: ["Nothing","Hail","Report"]
+        input name: "Button_Turn_Mode_param_120", type: "enum", title: "External Button Turn Mode (param 120)", description: "(0:Momentary button mode, 1: 2 state switch mode, 2: 3 way switch mode, 255: Unidentified mode, Other: ignore)", required: true, options: ["Momentary","Two State Switch","Three Way Switch","Unidentified Mode"]
     }
   
 
@@ -79,11 +79,8 @@ metadata {
 		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
 			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
-		standardTile("blink", "device.alarm", inactiveLabel: false, decoration: "flat") {
-			state "default", label:"", action:"alarm.strobe", icon:"st.secondary.strobe", backgroundColor:"#ffffff"
-		}
 		main(["switch"])
-        details(["switch","blink","refresh"])
+        details(["switch","refresh"])
 	}
  }
 
@@ -118,78 +115,21 @@ def on() {
     //reset alarm trigger
     state.alarmTriggered = 0
 	//Don't request a config report when advanced reporting is enabled
-	if (settings.param80 in ["Hail","Report"]) zwave.basicV1.basicSet(value: 0xFF).format()
-    else delayBetween([zwave.basicV1.basicSet(value: 0xFF).format(), zwave.basicV1.basicGet().format()], 5000)
+	if (settings.Notifications_param_80 in ["Hail","Report"]) zwave.basicV1.basicSet(value: 0xFF).format()
+    else delayBetween([zwave.basicV1.basicSet(value: 0xFF).format(), zwave.basicV1.basicGet().format()], 50)
 }
 
 def off() {
-    //log.debug "at:${state.alarmTriggered} swf:${state.stateWhenFlashed}"
-    
-    //override alarm off command from smartApps
-    if (state.alarmTriggered == 1 && state.stateWhenFlashed == 1) {
-    	state.alarmTriggered = 0
-    } else {
-    	//Don't request a config report when advanced reporting is enabled
-    	if (settings.param80 in ["Hail","Report"]) zwave.basicV1.basicSet(value: 0x00).format()
-		else delayBetween ([zwave.basicV1.basicSet(value: 0x00).format(),  zwave.basicV1.basicGet().format()], 5000)
-    }
+    //Don't request a config report when advanced reporting is enabled
+    if (settings.Notifications_param_80 in ["Hail","Report"]) zwave.basicV1.basicSet(value: 0x00).format()
+	else delayBetween ([zwave.basicV1.basicSet(value: 0x00).format(),  zwave.basicV1.basicGet().format()], 50)
+
 }
 
 def refresh() {
      return zwave.basicV1.basicGet().format()
 }
 
-//built in flasher mode
-def flash(type) {
-	if (!type) type = settings.blinker
-	//AEON blink parameters
-	//1: blink duration in seconds 1-255
-    //2: cycle time in .1 seconds (50% duty cycle) 1-255
-    def pBlink = []
-    def boolean isOn = device.currentValue("switch") == "on"
-	
-    if (isOn) state.stateWhenFlashed = 1
-    else state.stateWhenFlashed = 0
-	
-    //switch (settings.blinker) {
-    switch (type) {
-		case "Flasher":
-        	pBlink.add(10)
-            pBlink.add(10)
-            break
-		case "Strobe":
-            pBlink.add(3)
-            pBlink.add(2)
-            break
-		default: //Blink
-			if (isOn) {
-            	pBlink.add(1)
-            	pBlink.add(5)
-            }
-            else {
-            	pBlink.add(1)
-            	pBlink.add(20)
- 			}
-            break
-	}
-	zwave.configurationV1.configurationSet(configurationValue: pBlink, parameterNumber: 2, size: 2).format()
-}
-
-//alarm methods
-def strobe() {
-	state.alarmTriggered = 1
-	flash(settings.blinker)
-}
-
-def siren() {
-	state.alarmTriggered = 1
-	flash(settings.blinker)
-}
-
-def both()	{
-	state.alarmTriggered = 1
-	flash(settings.blinker)
-}
 
 //capture preference changes
 def updated() {
@@ -197,41 +137,47 @@ def updated() {
     
     //get requested reporting preferences
     Short p80
-    switch (settings.param80) {
-		case "Off":
+    switch (settings.Notifications_param_80) {
+		case "Nothing":
 			p80 = 0
             break
 		case "Hail":
 			p80 = 1
             break
+		case "Report":
+			p80 = 2
+            break
 		default:
-			p80 = 2	//Report
+			p80 = 0 // Nothing
             break
 	}    
     
 	//get requested switch function preferences
     Short p120
-    switch (settings.param120) {
+    switch (settings.Button_Turn_Mode_param_120) {
 		case "Momentary":
 			p120 = 0
             break
-		case "Three Way":
+		case "Two State Switch":
+			p120 = 1
+            break
+		case "Three Way Switch":
 			p120 = 2
             break
 		default:
-			p120 = 1	//Toggle
+			p120 = 255 // Unidentified mode
             break
 	}    
   
 	//update if the settings were changed
-    if (p80 != state.param80)	{
+    if (p80 != state.Notifications_param_80)	{
     	//log.debug "update 80:${p80}"
-        state.param80 = p80 
+        state.Notifications_param_80 = p80 
         return response(zwave.configurationV1.configurationSet(parameterNumber: 80, size: 1, configurationValue: [p80]).format())
     }
-	if (p120 != state.param120)	{
+	if (p120 != state.Button_Turn_Mode_param_120)	{
     	//log.debug "update 120:${p120}"
-        state.param120 = p120
+        state.Button_Turn_Mode_param_120 = p120
         return response(zwave.configurationV1.configurationSet(parameterNumber: 120, size: 1, configurationValue: [p120]).format())
     }
 
@@ -239,9 +185,8 @@ def updated() {
 }
 
 def configure() {
-	settings.param80 = "Report"
-    settings.param120 = "Toggle"
-    settings.blinker = "Blink"
+	settings.Notifications_param_80 = "Report"
+    settings.Button_Turn_Mode_param_120 = "Toggle"
 	delayBetween([
 		zwave.configurationV1.configurationSet(parameterNumber: 80, size: 1, configurationValue: 2).format(),
 		zwave.configurationV1.configurationSet(parameterNumber: 120, size: 1, configurationValue: 1).format()
